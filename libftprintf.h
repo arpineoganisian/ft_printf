aa80e1a339c9b69f10a1f434e4a55f990456eeab
buhankahleba <74070665+buhankahleba@users.noreply.github.com>
2020-11-28 14:43:50 +0300

    Add files via upload

---
+++ libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwoodwri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 17:59:27 by hwoodwri          #+#    #+#             */
+/*   Updated: 2020/11/26 18:01:39 by hwoodwri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+#define LIBFTPRINTF_H
+
+# include <stdarg.h> //va_list
+# include <unistd.h> //write
+# include <stdlib.h> //malloc, free
+#endif
