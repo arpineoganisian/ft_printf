f26906903df98ee3cb101ff393058dfb2d1dbdc5
Herminia Woodwright <hwoodwri@ev-m6.kzn.21-school.ru>
2020-12-02 21:56:15 +0300

    printf

--- type_percent.c
+++ type_percent.c
@@ -6,7 +6,7 @@
 /*   By: hwoodwri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 15:49:04 by hwoodwri          #+#    #+#             */
-/*   Updated: 2020/12/02 21:49:52 by hwoodwri         ###   ########.fr       */
+/*   Updated: 2020/12/02 21:55:04 by hwoodwri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void type_percent(t_list *list)
 					ft_putchar_len('0', list);
 			else
 				while (list->width-- > 1)	
-					ft_putchar(' ', list);
+					ft_putchar_len(' ', list);
 		}
 		ft_putchar_len('%', list);
 		if ((list->minus && list->zero) || (list->minus && !list->zero))
 			while (list->width-- > 1)	
-				ft_putchar_len(' , list);
+				ft_putchar_len(' ', list);
 	}
     else
         ft_putchar_len('%', list);
